7f0649ef29bf363b6816f59c25afbbcd74cc30d7
JordanML28 <117130747+JordanML28@users.noreply.github.com>
2025-05-17 23:07:02 -0400

    Requerimiento 1 - 2 Hacer que la prueba pase

--- src/main/java/com/ucb/tdd/TicTacToe.java
+++ src/main/java/com/ucb/tdd/TicTacToe.java
@@ -2,5 +2,15 @@ package com.ucb.tdd;
 
 public class TicTacToe {
 
-    
+    private char[][] board = new char[3][3];
+
+    public void placePiece(int x, int y, char player) {
+        //Jordan
+        // Forzamos que falle en coordenadas inválidas
+        if (x > 2) {
+            throw new RuntimeException("Coordenada X inválida");
+        }
+
+        board[x][y] = player; // colocamos la pieza sin más validaciones
+    }
 }
