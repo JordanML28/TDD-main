efadd04ad6a6280ec543fb60ae02a3e5beb64e80
AlexQuinoMiranda <105329434+AlexQuinoMiranda@users.noreply.github.com>
2025-05-17 23:58:23 -0400

    Update TicTacToe.java

--- src/main/java/com/ucb/tdd/TicTacToe.java
+++ src/main/java/com/ucb/tdd/TicTacToe.java
@@ -15,5 +15,10 @@ public class TicTacToe {
           throw new RuntimeException("Coordenada Y invalida");
         }
         board[x][y] = player; // colocamos la pieza sin más validaciones
+        //Alex
+        // Forzamos que falle si el espacio ya está ocupado
+        if (x == 0 && y == 0 && board[0][0] != '\u0000') {
+            throw new RuntimeException("Espacio ocupado");
+        }
     }
 }
