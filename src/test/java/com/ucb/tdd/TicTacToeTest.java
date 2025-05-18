c6bf9759f5c51b4ff3a7b8f3b6449396e5b7076a
jackelinss <124317565+jackelinss@users.noreply.github.com>
2025-05-17 22:10:11 -0400

    Primera prueba Jackelin

--- src/test/java/com/ucb/tdd/TicTacToeTest.java
+++ src/test/java/com/ucb/tdd/TicTacToeTest.java
@@ -3,14 +3,14 @@ package com.ucb.tdd;
 import org.junit.Test;
 
 public class TicTacToeTest {
-//Jordan
+//Jackelin
     @Test(expected = RuntimeException.class)
     public void cuandoPiezaFueraDelEjeX_lanzarExcepcion() {
         // 1. Preparación
         TicTacToe juego = new TicTacToe();
 
         // 2. Lógica
-        juego.placePiece(3, 0, 'X'); // posición inválida en X
+        juego.placePiece(0, 3, 'X'); // posición inválida en X
 
         // 3. Verificación (automática por expected = RuntimeException)
     }
