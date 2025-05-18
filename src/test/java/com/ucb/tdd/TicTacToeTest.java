b816c0fdf04408d31353b3b9d001b778d0c3bdd0
JordanML28 <117130747+JordanML28@users.noreply.github.com>
2025-05-17 22:43:00 -0400

    Requerimiento 1 -  1.- Escribir una prueba que falle v2

--- src/test/java/com/ucb/tdd/TicTacToeTest.java
+++ src/test/java/com/ucb/tdd/TicTacToeTest.java
@@ -3,15 +3,16 @@ package com.ucb.tdd;
 import org.junit.Test;
 
 public class TicTacToeTest {
-//Jackelin
+//Jordan 
     @Test(expected = RuntimeException.class)
     public void cuandoPiezaFueraDelEjeX_lanzarExcepcion() {
         // 1. Preparación
         TicTacToe juego = new TicTacToe();
 
         // 2. Lógica
-        juego.placePiece(0, 3, 'X'); // posición inválida en X
+        juego.placePiece(3, 0, 'X'); // posición inválida en X
 
         // 3. Verificación (automática por expected = RuntimeException)
     }
+
 }
